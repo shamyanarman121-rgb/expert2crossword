10bf2e8b39ba0c94b24fc54855b456b11cf8a63d
shamyanarman121-rgb <shamyanarman121@gmail.com>
2026-03-19 18:11:32 +0400

    Add files via upload

---
+++ src/controllers/dataController.js
@@ -0,0 +1,24 @@
+const axios = require('axios');
+
+const processData = async (req, res) => {
+  try {
+    const data = req.body;
+    console.log("Data received from client:", data);
+    
+    // Replace with the actual external API URL
+    const externalApiUrl = 'https://hook.eu2.make.com/yncf934hhawechbyysh5954wpqq3x0f6'; 
+
+    const response = await axios.post(externalApiUrl, {
+      ...data,
+    });
+
+    res.status(200).json({ message: 'Data processed successfully', response: response.data });
+  } catch (error) {
+    console.error("Error processing data:", error.message);
+    res.status(500).json({ message: 'Internal server error', error: error.message });
+  }
+};
+
+module.exports = {
+  processData,
+};
