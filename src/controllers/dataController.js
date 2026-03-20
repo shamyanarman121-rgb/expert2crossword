1dd5ef969054a24502e4d863f5019a6896b29cba
shamyanarman121-rgb <shamyanarman121@gmail.com>
2026-03-20 11:19:04 +0400

    Update external API URL in dataController

--- src/controllers/dataController.js
+++ src/controllers/dataController.js
@@ -6,7 +6,7 @@ const processData = async (req, res) => {
     console.log("Data received from client:", data);
     
     // Replace with the actual external API URL
-    const externalApiUrl = 'https://hook.eu2.make.com/yncf934hhawechbyysh5954wpqq3x0f6'; 
+    const externalApiUrl = 'https://hook.eu2.make.com/h15yjqp63i849cmah8rh15378it6dwky'; 
 
     const response = await axios.post(externalApiUrl, {
       ...data,
